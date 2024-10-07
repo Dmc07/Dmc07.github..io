344a2c91b2644d388dce77c211d076b3a43eda31
Dmc07 <mccoyd613@gmail.com>
2024-10-07 14:58:51 +0000

    Platforms

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,8 +38,18 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
+    createPlatform(240, 650, 50, 350)
+    createPlatform(400, 200, 50, 320)
+    createPlatform(600, 200, 50, 300)
+    createPlatform(580, 630, 50, 50)
+    createPlatform(470, 500, 50, 50)
+    createPlatform(570, 380, 60, 30)
+    createPlatform(580, 280, 50, 30)
+    createPlatform(430, 200, 50, 30)
+    createPlatform(900, 500, 50, 30)
+    createPlatform(750, 500, 50, 30)
+    createPlatform(1200, 300, 50, 30)
+    createPlatform(1080, 400, 50, 30)
 
     
     // TODO 2
@@ -47,7 +57,9 @@ $(function () {
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
+    createCollectable("max", 570, 380); // creates a "max" collectable at the coordinates(570, 380), falling with default gravity and bouncing with default bounce %
+    createCollectable("database", 410, 150);
+    createCollectable("kennedi", 1200, 250); // creates a "database" collectable at the coordinates(410,150), falling with default gravity and bouncing with default bounce %
 
 
     
@@ -56,7 +68,10 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+    createCannon("bottom", 800, 500); // cannon on bottom wall, 800px down, shooting once every six second
+    createCannon("left", 120, 780); // cannon on left wall, 100px left, shooting once every seven seconds
+    createCannon("bottom", 1000, 470); // cannon on bottom wall, 1000px down,
+    
 
 
     
